1f52065039d87afb3b13fc18ee5e0c17d6dfa0ae
eatthepear <thelordofpear@gmail.com>
2023-12-29 00:56:59 -0500

    allow gen 9 starters

--- src/battle_setup.c
+++ src/battle_setup.c
@@ -1079,7 +1079,7 @@ u8 GetSpecialBattleTransition(s32 id)
 
 void ChooseStarter(void)
 {
-    u16 randomSeed = (Random() % 8) + (Random() % 8) * 8+ (Random() % 8) * 64;
+    u16 randomSeed = (Random() % 9) + (Random() % 9) * 9 + (Random() % 9) * 81;
     VarSet(VAR_RAND_STARTER_SEED, randomSeed);
     SetMainCallback2(CB2_ChooseStarter);
     gMain.savedCallback = CB2_GiveStarter;
