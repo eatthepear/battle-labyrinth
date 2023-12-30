e6a3912d9f8b7d69688954ea4b36c0ca041620ca
eatthepear <thelordofpear@gmail.com>
2023-12-29 23:11:49 -0500

    encounter rates adjusted

--- src/wild_encounter.c
+++ src/wild_encounter.c
@@ -210,16 +210,14 @@ u8 ChooseWildMonIndex_Land(void)
         wildMonIndex = 8;
     else if (rand >= ENCOUNTER_CHANCE_LAND_MONS_SLOT_8 && rand < ENCOUNTER_CHANCE_LAND_MONS_SLOT_9)
         wildMonIndex = 9;
-    else if (rand >= ENCOUNTER_CHANCE_LAND_MONS_SLOT_9 && rand < ENCOUNTER_CHANCE_LAND_MONS_SLOT_10)
-        wildMonIndex = 10;
     else
-        wildMonIndex = 11;
+        wildMonIndex = 10;
 
     if (LURE_STEP_COUNT != 0 && (Random() % 10 < 2))
         swap = TRUE;
 
     if (swap)
-        wildMonIndex = 11 - wildMonIndex;
+        wildMonIndex = 10 - wildMonIndex;
 
     return wildMonIndex;
 }
@@ -236,14 +234,16 @@ u8 ChooseWildMonIndex_WaterRockHeadbutt(void)
         wildMonIndex = 1;
     else if (rand >= ENCOUNTER_CHANCE_WATER_MONS_SLOT_1 && rand < ENCOUNTER_CHANCE_WATER_MONS_SLOT_2)
         wildMonIndex = 2;
-    else
+    else if (rand >= ENCOUNTER_CHANCE_WATER_MONS_SLOT_2 && rand < ENCOUNTER_CHANCE_WATER_MONS_SLOT_3)
         wildMonIndex = 3;
+    else
+        wildMonIndex = 4;
 
     if (LURE_STEP_COUNT != 0 && (Random() % 10 < 2))
         swap = TRUE;
 
     if (swap)
-        wildMonIndex = 3 - wildMonIndex;
+        wildMonIndex = 4 - wildMonIndex;
 
     return wildMonIndex;
 }
