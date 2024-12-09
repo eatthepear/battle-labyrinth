e61614932121c613c66729a1452b81bcbc1eeb84
eatthepear <thelordofpear@gmail.com>
2024-12-08 21:54:46 -0500

    [PBL] ported trainer classes and sprites

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -7969,6 +7969,7 @@ static u32 GetTrainerMoneyToGive(u16 trainerId)
     u32 lastMonLevel = 0;
     u32 moneyReward;
     u8 trainerMoney = 0;
+    u8 numMons = 0;
 
     if (trainerId == TRAINER_SECRET_BASE)
     {
@@ -7980,7 +7981,9 @@ static u32 GetTrainerMoneyToGive(u16 trainerId)
         if (party == NULL)
             return 20;
         lastMonLevel = party[GetTrainerPartySizeFromId(trainerId) - 1].lvl;
-        trainerMoney = gTrainerClasses[GetTrainerClassFromId(trainerId)].money ?: 5;
+        numMons = GetTrainerPartySizeFromId(trainerId);
+        trainerMoney = (gTrainerClasses[GetTrainerClassFromId(trainerId)].money ?: 2) * numMons;
+        // trainerMoney = gTrainerClasses[GetTrainerClassFromId(trainerId)].money ?: 5;
 
         if (gBattleTypeFlags & BATTLE_TYPE_TWO_OPPONENTS)
             moneyReward = 4 * lastMonLevel * gBattleStruct->moneyMultiplier * trainerMoney;
