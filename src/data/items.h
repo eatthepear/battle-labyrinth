7acbf6ffefbf1205146b039ba377e316f08c663b
eatthepear <thelordofpear@gmail.com>
2024-12-25 12:03:19 -0500

    [PBL] zone 6 max

--- src/data/items.h
+++ src/data/items.h
@@ -10545,7 +10545,7 @@ const struct Item gItemsInfo[] =
     [ITEM_TM_TRAILBLAZE] =
     {
         .name = _("TM09"),
-        .price = 3000,
+        .price = 3500,
         .description = COMPOUND_STRING(
             "The user attacks\n"
             "suddenly, raising\n"
@@ -11065,7 +11065,7 @@ const struct Item gItemsInfo[] =
     [ITEM_TM_FLAME_CHARGE] =
     {
         .name = _("TM43"),
-        .price = 3000,
+        .price = 3500,
         .description = COMPOUND_STRING(
             "Attacks in a cloak\n"
             "of flames. Raises\n"
@@ -11275,7 +11275,7 @@ const struct Item gItemsInfo[] =
     [ITEM_TM_CHARGE_BEAM] =
     {
         .name = _("TM57"),
-        .price = 3000,
+        .price = 3500,
         .description = COMPOUND_STRING(
             "Attacks with an\n"
             "electric charge,\n"
@@ -11860,7 +11860,7 @@ const struct Item gItemsInfo[] =
     [ITEM_TM_POWER_UP_PUNCH] =
     {
         .name = _("TM96"),
-        .price = 3000,
+        .price = 3500,
         .description = COMPOUND_STRING(
             "Strikes the foe\n"
             "with its fists,\n"
