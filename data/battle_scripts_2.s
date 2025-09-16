e665c5fb8953a2ab7be24ee47b90f0a677b188ca
eatthepear <thelordofpear@gmail.com>
2025-09-15 22:45:57 -0400

    [PBL] disabled show pokedex page after catch to fix freeze with custom battle backgrounds

--- data/battle_scripts_2.s
+++ data/battle_scripts_2.s
@@ -189,7 +189,7 @@ BattleScript_TryPrintCaughtMonInfo:
 	printstring STRINGID_PKMNDATAADDEDTODEX
 	waitstate
 	setbyte gBattleCommunication, 0
-	displaydexinfo
+	// displaydexinfo
 BattleScript_TryNicknameCaughtMon::
 	printstring STRINGID_GIVENICKNAMECAPTURED
 	waitstate
