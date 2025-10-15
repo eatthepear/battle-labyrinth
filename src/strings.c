c72a797a8eda3f487d060476818f6f88142b40d5
eatthepear <thelordofpear@gmail.com>
2025-10-14 23:07:33 -0400

    [PBL] checks that you got items + cleanup

--- src/strings.c
+++ src/strings.c
@@ -1412,7 +1412,7 @@ const u8 gText_SideQuestName_Berries[] = _("Berry Investments");
 
 //descriptions
 const u8 gText_SideQuestDesc_Filler[] = _("Filler Description");
-const u8 gText_SideQuestDesc_PBL[] = _("Complete all 50 Zones without losing\na single battle!\nSo far you've completed {STR_VAR_1} Zones!");
+const u8 gText_SideQuestDesc_PBL[] = _("Complete all 50 Zones!\nSo far you've completed {STR_VAR_1} Zones!");
 const u8 gText_SideQuestDesc_Signs[] = _("Read all the signs in the game!");
 const u8 gText_SideQuestDesc_Saves[] = _("Save the game 50 times!\nSo far you've saved the game {STR_VAR_1} times!");
 const u8 gText_SideQuestDesc_Pokedex[] = _("See all the Pokémon in the Pokédex!\nSo far you've seen {STR_VAR_1} Pokémon!");
