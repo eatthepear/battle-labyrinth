5e7ddfbc3608f9af5c875f036e2b6d75efb1a4d3
eatthepear <thelordofpear@gmail.com>
2025-12-18 16:16:43 -0500

    [PBL] galactic encounter music

--- src/battle_setup.c
+++ src/battle_setup.c
@@ -1551,6 +1551,9 @@ void PlayTrainerEncounterMusic(void)
         case TRAINER_ENCOUNTER_MUSIC_RICH:
             music = MUS_ENCOUNTER_RICH;
             break;
+        case TRAINER_ENCOUNTER_MUSIC_GALACTIC:
+            music = MUS_DP_ENCOUNTER_GALACTIC;
+            break;
         default:
             music = MUS_ENCOUNTER_SUSPICIOUS;
         }
